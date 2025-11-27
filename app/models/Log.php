8faeafaec4ae127ef78bf5cf9f90dc59101095a9
A1liiii <mhmmdalimirza@gmail.com>
2025-11-27 10:52:58 +0700

    perbaiki model Log.php ke format postgre

--- app/models/Log.php
+++ app/models/Log.php
@@ -7,11 +7,18 @@ class Log {
     private $conn;
 
     public function __construct() {
-        $this->conn = Database::connect();
+        $this->conn = Database::connect(); // PDO
     }
 
     public function last($limit = 10) {
-        $sql = "SELECT * FROM log_activity ORDER BY created_at DESC LIMIT $limit";
-        return $this->conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+        $sql = "SELECT * FROM log_activity 
+                ORDER BY waktu DESC 
+                LIMIT :limit";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':limit', (int)$limit, PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
