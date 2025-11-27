51b5e04c37a9a42586a0913f60d40d638d068e1a
A1liiii <mhmmdalimirza@gmail.com>
2025-11-28 06:08:09 +0700

    tambah file routes

---
+++ app/Config/routes.php
@@ -0,0 +1,6 @@
+<?php
+$routes->get('/proyek', 'Proyek::index');
+$routes->get('/proyek/tambah', 'Proyek::create');
+$routes->post('/proyek/store', 'Proyek::store');
+$routes->get('/proyek/detail/(:num)', 'Proyek::detail/$1');
+?>
