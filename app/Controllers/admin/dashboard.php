b565776107ab24283b96bcb058e7b1face62b2f0
A1liiii <mhmmdalimirza@gmail.com>
2025-11-28 07:02:35 +0700

    fix db(db to conn) dan ganti sinkron kan fungsi Approval

--- app/Controllers/admin/dashboard.php
+++ app/Controllers/admin/dashboard.php
@@ -2,9 +2,9 @@
 
 require_once __DIR__ . '/../../models/Mahasiswa.php';
 require_once __DIR__ . '/../../models/User.php';
-// require_once __DIR__ . '/../../models/Approval.php';
+require_once __DIR__ . '/../../models/Approval.php';
 require_once __DIR__ . '/../../models/Dokumentasi.php';
-// require_once __DIR__ . '/../../models/Log.php';
+require_once __DIR__ . '/../../models/Log.php';
 
 class DashboardController {
 
@@ -13,17 +13,17 @@ class DashboardController {
         // === Panggil model ===
         $Mahasiswa = new Mahasiswa();
         $User = new User();
-        // $Registration = new Approval();
-        // $pendingReg = $Registration->countPending();
+        $Registration = new Approval();
+        $pendingReg = $Registration->getPending();
         $Dokumentasi = new Dokumentasi();
-        // $Log = new Log();
+        $Log = new Log();
 
         // === Ambil data ===
         $totalMahasiswa   = $Mahasiswa->count();
         $totalUser        = $User->count();
-        // $pendingReg       = $User->countPending();
+        $pendingReg       = $User->countPending();
         $totalDokumentasi = $Dokumentasi->count();
-        // $recentActivity   = $Log->last(10);
+        $recentActivity   = $Log->last(10);
 
         // === Data untuk view ===
         $title  = 'Dashboard - IVSS';
