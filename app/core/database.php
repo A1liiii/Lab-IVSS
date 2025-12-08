956204864ef1357c80367bb2d7d0805ccf1ea82d
rastaanra <rastaaci82@gmail.com>
2025-12-08 20:24:23 +0700

    ganti password database

---
+++ app/core/database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database {
+    public static function connect() {
+        $host = "localhost";
+        $port = "5432"; 
+        $user = "postgres"; 
+        $pass = "258369";        
+        $db   = "labivss";
+
+        try {
+            $dsn = "pgsql:host=$host;port=$port;dbname=$db;";
+            $conn = new PDO($dsn, $user, $pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            die("FAILED TO CONNECT DB: " . $e->getMessage());
+        }
+    }
+}
